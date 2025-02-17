89f6f5cad627d6cb4aae8b723eb3b66e5c7bd0cf
Juan Garrido <juangarridocaballero@gmail.com>
2025-02-17 18:33:06 +0100

    Add assets

---
+++ assets/inc-highlight/DIGESTS.md
@@ -0,0 +1,33 @@
+## Subresource Integrity
+
+If you are loading Highlight.js via CDN you may wish to use [Subresource Integrity](https://developer.mozilla.org/en-US/docs/Web/Security/Subresource_Integrity) to guarantee that you are using a legimitate build of the library.
+
+To do this you simply need to add the `integrity` attribute for each JavaScript file you download via CDN. These digests are used by the browser to confirm the files downloaded have not been modified.
+
+```html
+<script
+  src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.10.0/highlight.min.js"
+  integrity="sha384-pGqTJHE/m20W4oDrfxTVzOutpMhjK3uP/0lReY0Jq/KInpuJSXUnk4WAYbciCLqT"></script>
+<!-- including any other grammars you might need to load -->
+<script
+  src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.10.0/languages/go.min.js"
+  integrity="sha384-Mtb4EH3R9NMDME1sPQALOYR8KGqwrXAtmc6XGxDd0XaXB23irPKsuET0JjZt5utI"></script>
+```
+
+The full list of digests for every file can be found below.
+
+### Digests
+
+```
+sha384-R87hRh4kF8+iz2sB6FvLrfR0XZBohjFXeJKIXld1Eji2UVi+M2+OIgJKma/9Ko6u /es/languages/json.js
+sha384-QFDPNpqtrgZCuAr70TZJFM4VCY+xNnyGKwJw2EsWIBJOVcWAns9PHcLzecyDVv+x /es/languages/json.min.js
+sha384-8aLAoO/YSb+fhxwbMNNuUeq3x/FJJvtLwRq9nYpoyy4j85IpQURkoRySqJHBFlwu /es/languages/powershell.js
+sha384-LA+GHsDHAleSQDZ+9MEiJmTnrJ392Nkux5b1RIiGmzAkRo+4dwKsjS0z/pzCXIFr /es/languages/powershell.min.js
+sha384-psmmPlbfEWGyvRapexDqkVTgNz7Y1xvlGdLNWQSafI4GFQYFDXPZxVXH1laU4n6l /languages/json.js
+sha384-Bb6DhE3tUpBROwypL78TbhRUs9QbCt2GxcxVSYglt2l3MefrYkm4CfwjfWhRfQaX /languages/json.min.js
+sha384-/AFMsGSWoas65G5mSpnY9M1vnVP/9qhQW9yGZCpgbuL36JxWKX6MABh2hj6pc4yV /languages/powershell.js
+sha384-0u0NM3ve01ej9h9zRzZ/ztDGe1h07d6TStpNoJ4f/50I/vtoCsDHI2PfzDZSYz8q /languages/powershell.min.js
+sha384-rsis7q+YBxmYM8USuAJm1iDCjZAmAgAdqxiV5+2VptZKsMFW5TCfMtQx1K6fKQQb /highlight.js
+sha384-t1N9zzA3NvyMZQSr1V/UX5rHMZtdo4L1Fi45J+1JnYNQVmAIHYB3IJaFNq4d0Kb8 /highlight.min.js
+```
+
---
+++ assets/inc-monkey/js/monkey_helper.js
@@ -0,0 +1,318 @@
+//Sidebar collapse function
+$(document).ready(function(){
+	$("#sidebarCollapse").on('click', function(){
+		//adjust
+		$("#sidebar").toggleClass('active');
+	});
+	//adjust datatables
+	$('.collapse').on('shown.bs.collapse', function (e) {
+		var table = $(this).find('table');
+		if(table && $(table).attr('id')){
+			//initialise dataTable
+			var type = $(table).attr('type');
+			var tid = $(table).attr('id');
+			if ((!$.fn.DataTable.isDataTable('#'+tid)) && (type == 'asList')){
+				InitDatatableAsList(tid);
+			}
+			else if ((!$.fn.DataTable.isDataTable('#'+tid)) && (type == 'Default')){
+				InitDatatableNormal(tid);
+			}
+			if(tid){
+				$(tid).DataTable().columns.adjust().draw();
+				$(tid).DataTable().responsive.recalc();
+			}
+			//highlight code if any
+			if (typeof hljs !== 'undefined') {
+				var mybutton = $(table).find('button');
+				if(mybutton && $(mybutton).attr('title') && $(mybutton).attr('title')  == "showModal"){
+					var mybuttonId = $(mybutton).attr('data-bs-target');
+					if(mybuttonId){
+						var myobj = document.getElementById(mybuttonId.substring(1));
+						var codeId = $(myobj).find('code').attr('id');
+						if(codeId){
+							var myobj = document.getElementById(codeId);
+							var obj = JSON.parse(myobj.innerText);
+							myobj.textContent = JSON.stringify(obj, undefined, 4)
+							hljs.highlightElement(myobj);
+						}
+					}
+				}
+			}
+		}
+	});
+	//modal issue
+	//https://github.com/twbs/bootstrap/issues/41005
+	var modals = document.getElementsByClassName("modal");
+    if (modals) {
+		for (var i=0; i < modals.length; i++) {
+			modals[i].addEventListener('hide.bs.modal', () => {
+				if (document.activeElement instanceof HTMLElement) {
+					document.activeElement.blur();
+				}
+			});
+		}
+	}
+	//Init dashboard table
+	InitDatatableNormal("dashboard_table");
+	$("dashboard_table").DataTable().columns.adjust().draw();
+	$("dashboard_table").DataTable().responsive.recalc();
+});
+
+
+
+//Search filter
+
+$(".form-control.search-filter").on("input", function()  {
+	var searchVal = $(this).val().toLowerCase();
+	if ( searchVal != '' ) {
+		$('#Monkey365Data').children().not("#Monkey365GlobalFindings").addClass('d-none');
+		$('#Monkey365GlobalFindings').removeClass('d-none');
+		$('#Monkey365Findings').empty()
+		$('[id$="-findings"]').children().filter(function() {
+			if ($(this).find('.card-header').text().toLowerCase().indexOf(searchVal) > -1) {
+				let iterator = 0;
+				let clone = document.getElementById($(this).attr('id')).cloneNode(true);
+				//find table
+				let tabpane = clone.querySelectorAll('.tab-pane')
+				// add unique classes to each pane element
+				clone.querySelectorAll('.tab-pane').forEach((el, i) => {
+					var id = $(el).attr('id')
+					el.setAttribute('id', (id + 1));
+					el.classList.add('tab_' + iterator + i);
+					var _table = el.getElementsByTagName("table");
+					if(_table[0]){
+						var tableId = $(_table[0]).attr('id')
+						let _clone = document.getElementById($(_table[0]).attr('id')).cloneNode(true);
+						_clone.setAttribute('id', (tableId + 1));
+						$(el).html(_clone.outerHTML)
+					}
+				});
+
+				clone.querySelectorAll('.nav-link').forEach((el, i) => {
+					// update href attributes on each tab element to match panes
+					var id = $(el).attr('href')
+					id = id.substring(1);
+					el.setAttribute('href', ("#" + id + 1));
+					// initialize Bootstrap tabs on each tab element
+					const tab = new bootstrap.Tab(el)
+					el.addEventListener('click', event => {
+						event.preventDefault();
+						tab.show();
+					})
+				});
+				// append the clone
+				
+				document.querySelector('#Monkey365Findings').appendChild(clone);
+			}
+		})
+	}
+	else{
+		$('#Monkey365Findings').empty()
+		$('#Monkey365GlobalFindings').addClass('d-none');
+		show('monkey-main-dashboard')
+	}
+});
+
+//Change theme
+const toggleTheme = document.getElementById("toggleTheme");
+
+function changeTheme() {
+	if(this.classList.contains('bi-sun')){
+		//this.classList.toggle("bi-moon");
+		this.className = "bi bi-moon";
+		document.documentElement.setAttribute('data-theme','dark');
+	}
+	else{
+		this.className = "bi bi-sun";
+		document.documentElement.setAttribute('data-theme','light');
+	}
+}
+
+toggleTheme.addEventListener("click", changeTheme);
+
+$('.form-control.finding-filter').on("input", function() {
+	var searchVal = $(this).val().toLowerCase();
+	var id = $(this).parent().next('.monkey-card-data').attr('id');
+	$('#'+id).children().filter(function() {
+		$(this).toggle($(this).find('.card-header').text().toLowerCase().indexOf(searchVal) > -1)		
+	})
+});
+
+//Filter button
+$('.btn-filter').click(function(e) {
+	var value = $(this).attr('data-filter-name').toLowerCase();
+	var id = $(this).parent().next('.monkey-card-data').attr('id')
+	if(value == 'all'){
+		//remove d-none class
+		$('#'+id).find('.monkey-finding-card').removeClass('d-none');
+	}
+	else{
+		$('#'+id).find('.monkey-finding-card').removeClass('d-none');
+		$('#'+id).find('.monkey-finding-header').children().not('.finding-badge-'+value).closest('.monkey-finding-card').addClass('d-none');
+	}
+});
+
+jQuery.expr[':'].icontains = function(a, i, m) {
+  return jQuery(a).text().toUpperCase()
+      .indexOf(m[3].toUpperCase()) >= 0;
+};
+
+function openModal(message){
+	var modal = $('#monkey365error');
+	modal.find('.modal-body').html(message);
+	$('#monkey365error').modal('show');
+}
+
+function hasClass(element, className) {
+    return (' ' + element.className + ' ').indexOf(' ' + className+ ' ') > -1;
+}
+
+function show(elementID) {
+    var ele = document.getElementById(elementID);
+    if (!ele) {
+        openModal(elementID+" was not found");
+        return;
+    }
+	//Hide all except ID
+	$('#Monkey365Data').children().not("#accountInfo").addClass('d-none');
+	
+	//Show element
+	if($('#'+elementID)){
+        $('#'+elementID).removeClass('d-none');
+    }
+}
+
+function InitDatatableAsList(ID) {
+    var ele = document.getElementById(ID);
+    if (!ele) {
+        openModal("no such table ID "+ID);
+        return;
+    }
+    var oTable = $("#"+ID).DataTable({
+		lengthChange: false,
+		responsive: true,
+		aaSorting: [],
+		bSort: false,
+		info: false,
+		paging: false,
+		searching: false,
+        dom: 'Bfrtip',
+        fixedColumns: true,
+		fixedHeader: true,
+		//scrollCollapse: true,
+		layout: {
+			topStart: 'buttons',
+		},
+		buttons: {
+          dom: {
+            container:{
+              tag:'div',
+              className:'flexcontent'
+            },
+            buttonLiner: {
+              tag: null
+            }
+          },
+		  buttons: [
+			{
+				extend:    'excelHtml5',
+				text:      '<i class="bi bi-file-spreadsheet"></i>',
+				title:'Monkey365 Excel results',
+				titleAttr: 'Excel',
+				className: 'dtbtn dtbtn-app excel',
+				messageTop: 'https://github.com/silverhack/monkey365'
+			},
+			{
+				extend:    'csvHtml5',
+				text:      '<i class="bi bi-filetype-csv"></i>',
+				title:'Monkey365 CSV results',
+				titleAttr: 'CSV',
+				className: 'dtbtn dtbtn-app csv'
+			},
+			{
+				extend:    'print',
+				text:      '<i class="bi bi-printer-fill"></i>',
+				title:'Monkey365 Print results',
+				titleAttr: 'Print',
+				className: 'dtbtn dtbtn-app print',
+				messageTop: 'https://github.com/silverhack/monkey365',
+				customize: function ( win ) {
+						$(win.document.body)
+							.css( 'font-size', '10pt' ) 
+						$(win.document.body).find( 'table' )
+							.addClass( 'compact' )
+							.css( 'font-size', 'inherit' );
+					}
+			}
+		],
+		}
+	}).columns.adjust();
+}
+
+function InitDatatableNormal(ID) {
+    var ele = document.getElementById(ID);
+    if (!ele) {
+        openModal("no such table ID "+ID);
+        return;
+    }
+    var oTable = $("#"+ID).DataTable({
+		responsive: true,
+		layout: {
+			topStart: 'buttons',
+			topEnd: {
+				search: {
+					placeholder: 'Search here'
+				}
+			},
+			bottomEnd: {
+				paging: {
+					numbers: true,
+					firstLast: true
+				}
+			}
+		},
+        buttons: {
+          dom: {
+            container:{
+              tag:'div',
+              className:'flexcontent'
+            },
+            buttonLiner: {
+              tag: null
+            }
+          },
+		  buttons: [
+                    {
+                        extend:    'excelHtml5',
+                        text:      '<i class="bi bi-file-spreadsheet"></i>',
+                        title:'Monkey365 Excel results',
+                        titleAttr: 'Excel',
+                        className: 'dtbtn dtbtn-app excel',
+						messageTop: 'https://github.com/silverhack/monkey365'
+                    },
+                    {
+                        extend:    'csvHtml5',
+                        text:      '<i class="bi bi-filetype-csv"></i>',
+                        title:'Monkey365 CSV results',
+                        titleAttr: 'CSV',
+                        className: 'dtbtn dtbtn-app csv'
+                    },
+					{
+                        extend:    'print',
+                        text:      '<i class="bi bi-printer-fill"></i>',
+                        title:'Monkey365 Print results',
+                        titleAttr: 'Print',
+                        className: 'dtbtn dtbtn-app print',
+						messageTop: 'https://github.com/silverhack/monkey365',
+						customize: function ( win ) {
+								$(win.document.body)
+									.css( 'font-size', '10pt' ) 
+								$(win.document.body).find( 'table' )
+									.addClass( 'compact' )
+									.css( 'font-size', 'inherit' );
+							}
+                    },
+                ],
+		}
+	}).columns.adjust().responsive.recalc();
+}
